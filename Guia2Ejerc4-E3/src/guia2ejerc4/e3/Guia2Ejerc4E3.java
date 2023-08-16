f580c1a810c679a326230d6121df7b9ebd7f04df
Davidstafe <David.fmdrm@gmail.com>
2023-08-16 19:16:03 -0300

    cree nueva clase

--- Guia2Ejerc4-E3/src/guia2ejerc4/e3/Guia2Ejerc4E3.java
+++ Guia2Ejerc4-E3/src/guia2ejerc4/e3/Guia2Ejerc4E3.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package guia2ejerc4.e3;
 
 /**
@@ -11,9 +7,6 @@ package guia2ejerc4.e3;
  */
 public class Guia2Ejerc4E3 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         // TODO code application logic here
     }
